a07ef56ec83d03fd5349224d302a6f60ffebac8d
tominagayuuki <k021g1421@g.neec.ac.jp>
2022-12-12 09:35:57 +0900

    a

--- Object3d.cpp
+++ Object3d.cpp
@@ -341,7 +341,7 @@ void Object3d::LoadTexture()
 	ScratchImage scratchImg{};
 
 	// WICテクスチャのロード
-	result = LoadFromWICFile( L"Resources/tex1.png", WIC_FLAGS_NONE, &metadata, scratchImg);
+	result = LoadFromWICFile( L"Resources/blue.png", WIC_FLAGS_NONE, &metadata, scratchImg);
 	assert(SUCCEEDED(result));
 
 	ScratchImage mipChain{};
